fcaf8b76fb41cd698c6e82e120cc00a95389b8ff
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-08 22:11:24 -0300

    Aula de aplicação web

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados;
+using Entra21.CSharp.ClinicaVeterinaria.Servico;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
 {
     //Dois pontos Herança(mais pra frente)
     public class RacaController : Controller
     {
+        private readonly RacaServico _racaServico;
+
+        //Construtor: Objetivo construir o objeto de RacaController, com o mínimo necessário para o funcionamento correto
+        public RacaController(ClinicaVeterinariaContexto contexto)
+        {
+            _racaServico = new RacaServico(contexto);
+        }
+
         /// <summary>
         /// EndPoint que permite listar todas as raças
         /// </summary>
@@ -30,6 +40,8 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
             [FromQuery] string nome,
             [FromQuery] string especie)
         {
+            _racaServico.Cadastrar(nome, especie);
+
             return RedirectToAction("Index");
         }
             
