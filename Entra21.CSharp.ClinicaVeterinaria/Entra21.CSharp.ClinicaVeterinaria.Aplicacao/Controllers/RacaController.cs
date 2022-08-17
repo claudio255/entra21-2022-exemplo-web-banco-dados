1afeecb57d8ff9683f6e1ba7abbc0dc57344bc73
claudio255 <claudio.h.raimundo123@gmail.com>
2022-08-17 20:19:12 -0300

    Mudança de GET para POST na Action registrar e Action alterar

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -45,10 +45,10 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
         }
 
         [Route("/raca/registrar")]
-        [HttpGet]
+        [HttpPost]
         public IActionResult Registrar(
-            [FromQuery] string nome,
-            [FromQuery] string especie)
+            [FromForm] string nome,
+            [FromForm] string especie)
         {
             _racaServico.Cadastrar(nome, especie);
 
@@ -84,8 +84,8 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
         }
 
         [Route("/raca/alterar")]
-        [HttpGet]
-        public IActionResult Alterar([FromQuery] int id, [FromQuery] string nome, [FromQuery] string especie)
+        [HttpPost]
+        public IActionResult Alterar([FromForm] int id, [FromForm] string nome, [FromForm] string especie)
         {
             _racaServico.Alterar(id, nome, especie);
 
